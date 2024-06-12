6b4837979ccb47c5f9a16c467b1feec4ffaf1ff7
Amo-494 <amoskipkirui0@gmail.com>
2024-06-12 23:45:42 +0300

    Updated settings.py to use environment variables for email address

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,8 +16,9 @@ from pathlib import Path
 
 load_dotenv()
 
-EMAIL_HOST_PASSWORD=os.getenv('pnne sekp hpus sanj')
-PASSWORD=os.getenv('tommy')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+PASSWORD=os.getenv('PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,7 +144,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amoskipkirui0@gmail.com'
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
